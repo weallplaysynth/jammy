4b14ca3e94041f02e2246bcc1b6bafc7c24cd229
Antony Nasce <nasce@mac.com>
2026-01-01 12:07:49 +0000

    Setting up pages.

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,7 @@ import adapter from '@sveltejs/adapter-static';
 const dev = process.env.NODE_ENV === 'development';
 
 // Change this to your GitHub repo name when you create it, e.g. "waps-learn"
-const repoName = 'waps-learn';
+const repoName = 'jammy';
 
 export default {
   kit: {
